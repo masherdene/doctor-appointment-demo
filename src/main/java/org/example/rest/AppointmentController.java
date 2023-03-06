e11bed243282405fd96cdaca8fe9d15b79f2e495
masherdene <masherdene@gmail.com>
2023-03-06 21:57:57 +0800

    tentative initial commit

--- src/main/java/org/example/rest/AppointmentController.java
+++ src/main/java/org/example/rest/AppointmentController.java
@@ -1,12 +1,10 @@
 package org.example.rest;
 
-import org.example.infrastructure.entity.AppointmentEntity;
-import org.example.infrastructure.mapper.ModelEntityMapperImpl;
 import org.example.infrastructure.repository.AppointmentRepository;
 import org.example.model.Appointment;
+import org.example.usecases.CreateAppointment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
@@ -14,18 +12,25 @@ import java.util.List;
 @RequestMapping(value = "/index")
 public class AppointmentController {
 
-    private ModelEntityMapperImpl modelEntityMapper;
+//    private ModelEntityMapperImpl modelEntityMapper;
     private AppointmentRepository appointmentRepository;
 
-    @GetMapping("/{all}")
-    List<Appointment> getAllAppointment(){
-//        List<Appointment> appointment = modelEntityMapper.getAllAppointments();
+    @GetMapping("/{create}")
+    List<Appointment> getAllAppointment(int doctorId, int patientId, int treatmentId){
+        CreateAppointment createAppointment = new CreateAppointment();
+        Appointment appointment = createAppointment.execute(doctorId, patientId, treatmentId);
         return appointment;
     }
-
-    @GetMapping("/{id}")
-    AppointmentEntity getAppointmentById(@RequestParam Long appointmentId){
-//        AppointmentEntity appointmentEntity = appointmentRepository.findById(appointmentId);
-        return appointmentEntity;
-    }
+//
+//    @GetMapping("/{all}")
+//    List<Appointment> getAllAppointment(){
+////        List<Appointment> appointment = modelEntityMapper.getAllAppointments();
+//        return appointment;
+//    }
+//
+//    @GetMapping("/{id}")
+//    AppointmentEntity getAppointmentById(@RequestParam Long appointmentId){
+////        AppointmentEntity appointmentEntity = appointmentRepository.findById(appointmentId);
+//        return appointmentEntity;
+//    }
 }
